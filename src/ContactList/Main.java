c5aed1353a06f728b7bab532bf4eb097757cc94c
Henry Feng <henryfzh@outlook.com>
2023-10-08 08:50:00 -0700

    add frame

---
+++ src/ContactList/Main.java
@@ -0,0 +1,79 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.geometry.Insets;
+import javafx.scene.text.*;
+import java.io.*;
+import java.util.*;
+
+class Task extends HBox {
+
+    private Label index;
+    private TextField contactName;
+}
+
+class Footer extends HBox {
+
+    private Button addButton;
+    private Button delButton;
+    private Button view;
+
+    Footer() {
+        this.setPrefSize(500, 60);
+        this.setStyle("-fx-background-color: #F0F8FF;");
+        this.setSpacing(15);
+
+        // set a default style for buttons - background color, font size, italics
+        String defaultButtonStyle = "-fx-font-style: italic; -fx-background-color: #FFFFFF;  -fx-font-weight: bold; -fx-font: 11 arial;";
+    }
+}
+
+class Header extends HBox {
+
+    Header() {
+        this.setPrefSize(500, 60); // Size of the header
+        this.setStyle("-fx-background-color: #F0F8FF;");
+
+        Text titleText = new Text("To Do List"); // Text of the Header
+        titleText.setStyle("-fx-font-weight: bold; -fx-font-size: 20;");
+        this.getChildren().add(titleText);
+        this.setAlignment(Pos.CENTER); // Align the text to the Center
+    }
+}
+
+class AppFrame extends BorderPane {
+    private Header header;
+    private Footer footer;
+    private ScrollPane scroll;
+}
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        // Setting the Layout of the Window- Should contain a Header, Footer and the
+        // TaskList
+        AppFrame root = new AppFrame();
+
+        // Set the title of the app
+        primaryStage.setTitle("To Do List");
+        // Create scene of mentioned size with the border pane
+        primaryStage.setScene(new Scene(root, 500, 600));
+
+        // Make window non-resizable
+        primaryStage.setResizable(false);
+        // Show the app
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
